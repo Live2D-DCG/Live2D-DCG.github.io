c4d6b553c61968615ab4be7fa4f5d23cbb3ea4ff
Live2D-DCG <destinychildreddit@gmail.com>
2020-05-28 12:43:36 -0400

    Updated CDN Link

--- static/js/live2dsdk/ui.js
+++ static/js/live2dsdk/ui.js
@@ -35,11 +35,11 @@ function updateViewer(id) {
   if (diffView == "k") {
 	  viewer.src = 'viewerK.html?mN=' + selectedModel + '&size=' + size + '&mS=' + mS  + '&mX=' + mX  + '&mY=' + mY
 	  code = viewer.outerHTML.replace('viewerk.html', document.location.toString() + 'viewerk.html')
-	  download = "http://patch.dc.nextfloor.com/1.3.41.evm8ualcvub8/hd_android/asset/character/" + selectedModel +".pck"
+	  download = "http://patch.dc.nextfloor.com/1.3.44.1g8tn5m91idt/hd_android/asset/character/" + selectedModel +".pck"
   } else {
 	  viewer.src = 'viewer.html?mN=' + selectedModel + '&size=' + size + '&mS=' + mS  + '&mX=' + mX  + '&mY=' + mY
 	  code = viewer.outerHTML.replace('viewer.html', document.location.toString() + 'viewer.html')
-	  download = "http://patch.dcg.line.games/real_0.0.52_uzfouoivkfvy/hd_mobile/asset/character/" + selectedModel +".pck"
+	  download = "http://patch.dcg.line.games/real_0.0.54_7wuqjqf1szle/hd_mobile/asset/character/" + selectedModel +".pck"
   }
   
 
