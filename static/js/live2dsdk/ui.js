334ab180d31a2d6551082e26bf3733112d1fbca5
Live2D-DCG <DestinyChildReddit@gmail.com>
2020-10-20 21:22:13 -0400

    Update ui.js

--- static/js/live2dsdk/ui.js
+++ static/js/live2dsdk/ui.js
@@ -41,7 +41,7 @@ function updateViewer(id) {
   } else {
 	  viewer.src = 'viewer.html?mN=' + selectedModel + '&size=' + size + '&mS=' + mS  + '&mX=' + mX  + '&mY=' + mY
 	  code = viewer.outerHTML.replace('viewer.html', document.location.toString() + 'viewer.html')
-	  download = "ttp://patch.dcg.line.games/real_0.0.72.vjmsl18zxt3f/hd_mobile/asset/character/" + selectedModel +".pck"
+	  download = "http://patch.dcg.line.games/real_0.0.72.vjmsl18zxt3f/hd_mobile/asset/character/" + selectedModel +".pck"
 	  
   }
   
