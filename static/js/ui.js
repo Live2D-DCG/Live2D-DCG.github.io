fbffa29f2f1e067e0d31f4fd58b612894f97ea20
Live2D-DCG <destinychildreddit@gmail.com>
2021-06-21 11:30:02 -0400

    Update Child names

--- static/js/ui.js
+++ static/js/ui.js
@@ -7,8 +7,8 @@ var selectedModel,
 	diffView = "k"
 	
 
-var globalURL = "https://patch.dcg.line.games/real_0.0.96.aqagzjdrt384/";
-var krURL= "https://patch.dc.nextfloor.com/real_1.3.88.e7mvs2um5nkn/";
+var globalURL = "https://patch.dcg.line.games/real_0.0.97.prxn9cggqejh/";
+var krURL= "https://patch.dc.nextfloor.com/real_1.3.89.wly6mu5l7vfl/";
 
 
 function getSelectedIndex() {
