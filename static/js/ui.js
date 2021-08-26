7daa79757187c1390518cd79ba8b5a362d2a449e
Live2D-DCG <destinychildreddit@gmail.com>
2021-08-26 12:45:03 -0400

    08/26/21 Patch

--- static/js/ui.js
+++ static/js/ui.js
@@ -7,8 +7,8 @@ var selectedModel,
 	diffView = "k"
 	
 
-var globalURL = "https://patch.dcg.line.games/real_0.1.02.smiwnbxpy1nw/";
-var krURL= "https://patch.dc.nextfloor.com/real_1.3.95.eakrqbjxi9gn/";
+var globalURL = "https://patch.dcg.line.games/real_0.1.04.tnlelcmcvimr/";
+var krURL= "https://patch.dc.nextfloor.com/real_1.3.96.gln6vzybgzrq/";
 
 
 function getSelectedIndex() {
