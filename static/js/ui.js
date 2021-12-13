d54225e548bc19cf06d72ea04e0b5809952a0e9b
Live2D-DCG <59317528+Live2D-DCG@users.noreply.github.com>
2021-12-12 19:59:16 -0500

    Updated the pck's official links

--- static/js/ui.js
+++ static/js/ui.js
@@ -7,8 +7,8 @@ var selectedModel,
 	diffView = "k"
 	
 
-var globalURL = "https://patch.dcg.line.games/real_0.1.06.mofssmozovat/";
-var krURL= "https://patch.dc.nextfloor.com/real_1.3.98.7chm5wxe9vms/";
+var globalURL = "https://patch.dcg.line.games/real_0.1.13.e8ijtt9imumi/";
+var krURL= "https://patch.dc.nextfloor.com/real_1.4.14.s6mve3wru0en/";
 
 function getSelectedIndex() {
   return modelIds.indexOf(selectedModel)
