763bbdc59093d85803d3b5626bf448f20b5ce0e1
Live2D-DCG <59317528+Live2D-DCG@users.noreply.github.com>
2023-03-30 13:26:23 -0400

    03/30/23 GBL Patch
    New Units / Puppets:
    c186 - Erebus
    c203 - Harmonia
    c364_10 - Sytry Costume
    c381_10 - Sytry Costume
    c526 - Rani
    c527 - Reynard
    
    Visual Updates:
    c061_00 - Siren
    c061_01 -Siren
    c062_01 - Europa
    c187_00 - Thetis
    c187_01 - Thetis

--- static/js/ui.js
+++ static/js/ui.js
@@ -38,8 +38,8 @@ fetch(url,koreanOptions)
 
 */
 
-const globalURL = "https://patch.cdn.dcgl.shiftupcorp.com/real.171.8omtrncyfowt/"
-const krURL = "https://patch.cdn.dckr.shiftupcorp.com/real.110.cmm5qds4e454/"
+const globalURL = "https://patch.cdn.dcgl.shiftupcorp.com/real.175.wgpo53gv7pwb/"
+const krURL = "https://patch.cdn.dckr.shiftupcorp.com/real.125.vcap9hjgeasg/"
 
 function getSelectedIndex() {
   return modelIds.indexOf(selectedModel)
